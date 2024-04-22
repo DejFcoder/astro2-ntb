50428285ab18a37cb4d5fc3b89c0b3534cd97928
DejFcoder <trebatickydavid670@gmail.com>
2024-04-22 19:00:11 +0200

    netlify&vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
-});
+  adapter: vercel()
+});   
